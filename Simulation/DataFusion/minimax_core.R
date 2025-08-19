76da9c3627e2f5eda812836c93cfd3add4313318
Chang Liu <69181047+changliues@users.noreply.github.com>
2025-08-18 20:55:03 -0400

    Update minimax_core.R

--- Simulation/DataFusion/minimax_core.R
+++ Simulation/DataFusion/minimax_core.R
@@ -3,24 +3,22 @@ library(pracma)
 
 minimax_solve <- function(gram1, gram2, g1, g2, lambda1, lambda2){
   
-  """
-  This function returns the coefficients of the solution function h(r1),
-  which, along with f(r2), solves the following minimax optimization problem:
-
-  h = \argmin_{h} \max_f [
-      \mathbb{E} \big( f(r2) \cdot (h(r1) \cdot g1 + g2) - f(r2)^2 \big)
-      - \lambda_2 \|f\|_{RKHS}^2 + \lambda_1 \|h\|_{RKHS}^2
-  ]
-
-  Here, h belongs to the RKHS defined by the kernel function associated with 
-  the Gram matrix `gram1`, while f belongs to the RKHS defined by the kernel 
-  function associated with the Gram matrix `gram2`.
-
-  Returns:
-    list: A list `[alpha, beta]`, where:
-        - `alpha` is the coefficient vector of h in its RKHS.
-        - `beta` is the coefficient vector of f in its RKHS.
-  """
+  # This function returns the coefficients of the solution function h(r1),
+  # which, along with f(r2), solves the following minimax optimization problem:
+  # 
+  # h = \argmin_{h} \max_f [
+  #     \mathbb{E} \big( f(r2) \cdot (h(r1) \cdot g1 + g2) - f(r2)^2 \big)
+  #     - \lambda_2 \|f\|_{RKHS}^2 + \lambda_1 \|h\|_{RKHS}^2
+  # ]
+  # 
+  # Here, h belongs to the RKHS defined by the kernel function associated with 
+  # the Gram matrix `gram1`, while f belongs to the RKHS defined by the kernel 
+  # function associated with the Gram matrix `gram2`.
+  # 
+  # Returns:
+  #   list: A list `[alpha, beta]`, where:
+  #       - `alpha` is the coefficient vector of h in its RKHS.
+  #       - `beta` is the coefficient vector of f in its RKHS.
 
   n <- nrow(gram1)
 
@@ -35,16 +33,14 @@ minimax_solve <- function(gram1, gram2, g1, g2, lambda1, lambda2){
 
 score_nuisance_function <- function(h_values, gram2_score, g1, g2, lambda_score) {
 
-  """
-  Scores a fitted nuisance function h by solving the minimax problem with respect 
-  to the coefficient vector `beta` of function f, while keeping h(r1) fixed.  
-
-  The function f belongs to the RKHS associated with the Gram matrix `gram_score`, 
-  and the regularization parameter `lambda_2` is specified as the function input `lambda_score`.  
-
-  Returns:
-    float: The computed score, where a higher value indicates a better fit.
-  """
+  # Scores a fitted nuisance function h by solving the minimax problem with respect 
+  # to the coefficient vector `beta` of function f, while keeping h(r1) fixed.  
+  # 
+  # The function f belongs to the RKHS associated with the Gram matrix `gram_score`, 
+  # and the regularization parameter `lambda_2` is specified as the function input `lambda_score`.  
+  # 
+  # Returns:
+  #   float: The computed score, where a higher value indicates a better fit.
 
   n <- nrow(gram2_score)
   
